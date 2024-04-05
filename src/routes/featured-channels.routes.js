39c7ab8babb4e58017284027258a3705b9c92f4b
Swapnil Opash <sra.opash@gmail.com>
2024-04-05 11:37:55 +0530

    fixed channel api

--- src/routes/featured-channels.routes.js
+++ src/routes/featured-channels.routes.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const featuredChannels = require("../controllers/featuredChannels.controller");
 const authorize = require("../middleware/authorize");
 
-
 router.get("/", featuredChannels.getChannels);
 router.get("/get/:id", featuredChannels.findChannelById);
 router.get("/search-user", featuredChannels.getUsersByUsername);
@@ -13,12 +12,14 @@ router.get("/post/:id", featuredChannels.getPostDetails);
 router.post("/posts", featuredChannels.getVideos);
 router.post("/my-posts", featuredChannels.getChannelVideos);
 router.use(authorize.authorization);
+router.get("/get-channels/:id", featuredChannels.getChannelsByProfileId);
 router.get("/activate-channel", featuredChannels.channelsApprove);
 router.get("/feature/:id", featuredChannels.updateChannleFeature);
 router.post("/search-all", featuredChannels.searchAllData);
 router.post("/create-admin", featuredChannels.CreateSubAdmin);
 router.post("/get", featuredChannels.getAllChannels);
 router.post("/create-channel", featuredChannels.createChannel);
+router.put("/edit-channel/:id", featuredChannels.editChannel);
 router.delete("/leave", featuredChannels.removeFormChannel);
 router.delete("/:id", featuredChannels.deleteChannel);
 
