7ea29c953893d113675512d975d07a997f26f005
hirendanecha <hirendanecha@gmail.com>
2024-12-10 10:39:41 +0530

    add notification api

--- src/controllers/profile.controller.js
+++ src/controllers/profile.controller.js
@@ -313,3 +313,18 @@ exports.deleteGroup = async function (req, res) {
     return utils.send500(res, error);
   }
 };
+
+exports.readAllNotifications = async function (req, res) {
+  const { id } = req.params;
+  Profile.readAllNotifications(id, function (err) {
+    if (err) return utils.send500(res, err);
+    res.json({ error: false, message: "Notification updated successfully" });
+  });
+};
+
+exports.deleteAllNotification = function (req, res) {
+  Profile.deleteAllNotification(req.params.id, function (err, result) {
+    if (err) return utils.send500(res, err);
+    res.json({ error: false, message: "Notification deleted successfully" });
+  });
+};
